a48a7e1f66a90da5736792a29d8066ae1b4d8a0e
Zahir Yahya <theeYahya@gmail.com>
2018-02-03 13:09:02 -0800

    spellcheck all files together

--- spellcheck.sh
+++ spellcheck.sh
@@ -3,21 +3,18 @@
 # gets a list of all files with .md substr
 files=$(git ls-files | egrep .md)
 
-# for each file, run spellcheck
-for file in $files
-do
-    # spellcheck
-    result=$(spellchecker -f $file -d local-dictionary.txt -l en-US)
 
-    # print spellcheck result
-    echo "$result"
+# run spellcheck
+result=$(spellchecker -f $files -d local-dictionary.txt -l en-US)
 
-    # if the result has a warning substr, then failure
-    if [[ $result = *"warning"* ]]
-    then
-        printf "\nfile: $file failed spellcheck\n"
-        exit 1
-    fi
-done
+# print spellcheck result
+echo "$result"
+
+# if the result has a warning substr, then failure
+if [[ $result = *"warning"* ]]
+then
+    printf "\nfile: $file failed spellcheck\n"
+    exit 1
+fi
 
 exit 0
