5a9ff04e341623844fae4ac25f93ab7de052039c
Zahir Yahya <theeYahya@gmail.com>
2018-02-19 02:48:12 -0800

    Add spellcheck (fix inexorgame/inexor-core#9)
    * init travis.yml
    
    * created a local dictoray to store technical/ inexor-related terms that could fail spellcheck-cli
    
    * created script to run spell check
    
    * chanced permission so it can be executed
    
    * finished basic implementation
    
    * added comments
    
    * added terms to local dictionary
    
    * spellcheck all files together
    
    * added color to notify if spellcheck passed or failed
    
    * added/removed some terms
    
    * exclude example-post.md from being tested

---
+++ spellcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# gets a list of all files with .md substr
+files=$(git ls-files | git ls-files | egrep .md | grep -v "post-example.md")
+echo $files
+
+# run spellcheck
+result=$(spellchecker -f $files -d local-dictionary.txt -l en-US)
+
+# print spellcheck result
+echo "$result"
+
+# if the result has a warning substr, then failure
+if [[ $result = *"warning"* ]]
+then
+    printf "\n\e[31mFailed Spellcheck\e[0m\n"
+    exit 1
+fi
+
+printf "\n\e[32mPassed Spellcheck\e[0m\n"
+
+exit 0
